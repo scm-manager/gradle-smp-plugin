f1b11e72ea6eb6090c0deb68ceb74d59f1bfa39a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-12-21 09:17:46 +0100

    Support for core plugins and fix caching of typecheck

--- src/main/groovy/com/cloudogu/smp/UiTasks.groovy
+++ src/main/groovy/com/cloudogu/smp/UiTasks.groovy
@@ -3,6 +3,7 @@ package com.cloudogu.smp
 import com.moowork.gradle.node.yarn.YarnTask
 import com.moowork.gradle.node.NodeExtension
 import org.gradle.api.Project
+import org.gradle.api.GradleException
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.language.base.plugins.LifecycleBasePlugin
 
@@ -38,25 +39,37 @@ class UiTasks {
 
   private static void registerYarnInstall(Project project) {
     project.tasks.getByName('yarn_install').configure {
-      inputs.file("package.json")
-      inputs.file("yarn.lock")
-      outputs.dir("node_modules")
+      inputs.file('package.json')
+      inputs.file( project.rootProject.file('yarn.lock') )
+      outputs.dir( project.rootProject.file('node_modules') )
 
       description = "Install ui dependencies"
     }
   }
 
   private static void registerUITypeCheck(Project project) {
+    File marker = new File(project.buildDir, 'tmp/ui-typecheck/marker')
     project.tasks.register("ui-typecheck", YarnTask) {
       inputs.file("package.json")
-      inputs.file("yarn.lock")
+      inputs.file( project.rootProject.file('yarn.lock') )
       inputs.dir("src/main/js")
+      outputs.file(marker)
 
       args = ['run', 'typecheck']
       dependsOn("yarn_install")
 
       group = LifecycleBasePlugin.VERIFICATION_GROUP
       description = "Run typecheck"
+
+      doLast {
+        File directory = marker.getParentFile()
+        if (!directory.exists() && !directory.mkdirs()) {
+          throw new GradleException("failed to create directory ${directory}")
+        }
+        if (!marker.createNewFile()) {
+          throw new GradleException("failed to create marker file ${marker}")
+        }
+      }
     }
 
     project.tasks.getByName("check").configure {
@@ -67,7 +80,7 @@ class UiTasks {
   private static void registerUIBuild(Project project) {
     project.tasks.register("ui-bundle", YarnTask) {
       inputs.file("package.json")
-      inputs.file("yarn.lock")
+      inputs.file( project.rootProject.file('yarn.lock') )
       inputs.dir("src/main/js")
 
       outputs.dir("build/webapp/assets")
@@ -77,6 +90,7 @@ class UiTasks {
 
       group = BasePlugin.BUILD_GROUP
       description = "Assembles the plugin ui bundle"
+
     }
 
     project.afterEvaluate {
@@ -89,7 +103,7 @@ class UiTasks {
   private static void registerUITest(Project project) {
     project.tasks.register("ui-test", YarnTask) {
       inputs.file("package.json")
-      inputs.file("yarn.lock")
+      inputs.file( project.rootProject.file('yarn.lock') )
       inputs.dir("src/main/js")
 
       outputs.dir("build/jest-reports")
@@ -111,7 +125,7 @@ class UiTasks {
   private static void registerUIDeploy(Project project) {
     project.tasks.register("ui-deploy", YarnTask) {
       inputs.file("package.json")
-      inputs.file("yarn.lock")
+      inputs.file( project.rootProject.file('yarn.lock') )
       inputs.dir("src/main/js")
 
       args = ['run', 'deploy', project.version]
