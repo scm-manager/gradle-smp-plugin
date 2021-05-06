7f11b7178846e1363ffd2531e0e8c5ffb5943b96
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-05-06 11:52:47 +0200

    Fix sonarqube analysis for js-only projects

--- src/main/groovy/com/cloudogu/smp/AnalysisTasks.groovy
+++ src/main/groovy/com/cloudogu/smp/AnalysisTasks.groovy
@@ -12,7 +12,8 @@ class AnalysisTasks {
 
   static void configureSonarQube(Project project, SmpExtension extension, PackageJson packageJson) {
     project.plugins.apply('org.sonarqube')
-    if (packageJson.exists()) {
+    def javaSourcesExists = new File(project.rootDir, "src/main/java").exists()
+    if (packageJson.exists() && javaSourcesExists) {
       project.sonarqube {
         properties {
           property 'sonar.javascript.lcov.reportPaths', 'build/jest-reports/coverage/lcov.info'
@@ -20,6 +21,19 @@ class AnalysisTasks {
           property 'sonar.tests', 'src/test/java,src/main/js'
           property 'sonar.junit.reportPaths', 'build/test-results/test/,build/jest-reports/'
           property 'sonar.test.inclusions', 'src/**/*.test.ts,src/**/*.test.js,src/**/*.test.tsx,src/**/*.test.jsx,src/**/*Test.java,src/**/*ITCase.java'
+          property 'sonar.nodejs.executable', ".gradle/nodejs/node-v${Environment.NODE_VERSION}-${Environment.CI_OS}-${Environment.CI_ARCH}/bin/node"
+          property 'sonar.projectKey', "${extension.group}:${extension.getName(project)}"
+          properties extension.sonarProperties
+        }
+      }
+    } else if (packageJson.exists()) {
+      project.sonarqube {
+        properties {
+          property 'sonar.javascript.lcov.reportPaths', 'build/jest-reports/coverage/lcov.info'
+          property 'sonar.sources', 'src/main/js'
+          property 'sonar.tests', 'src/main/js'
+          property 'sonar.junit.reportPaths', 'build/jest-reports/'
+          property 'sonar.test.inclusions', 'src/**/*.test.ts,src/**/*.test.js,src/**/*.test.tsx,src/**/*.test.jsx'
           property 'sonar.nodejs.executable', ".gradle/nodejs/node-v${Environment.NODE_VERSION}-${Environment.CI_OS}-${Environment.CI_ARCH}/bin/node"
           property 'sonar.projectKey', "${extension.group}:${extension.getName(project)}"
           properties extension.sonarProperties
